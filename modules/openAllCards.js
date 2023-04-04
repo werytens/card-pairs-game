3f3a1ef2723f4f5a003fe0031fa4cbdd401731f9
Липатов Максим <103856799+greydatlenght@users.noreply.github.com>
2023-04-04 21:36:58 +0300

    Create openAllCards.js

---
+++ modules/openAllCards.js
@@ -0,0 +1,9 @@
+export function openAllCards() {
+    document.querySelectorAll(".cell").forEach(cell => cell.style.color = "white")
+
+    setTimeout(() => {
+        alert("Вы сдались! Перезапуск.")
+
+        location.reload()
+    }, 100)
+}
